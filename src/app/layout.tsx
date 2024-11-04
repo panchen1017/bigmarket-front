60c20a966bd53cf0e9ad2f9a5247a95c139f389d
panchen <hello@panchen.cn>
2024-11-05 00:52:24 +0800

    初次提交本地代码

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: '潘晨 - 金元宝营销平台 - 抽奖展示',
+  description: '金元宝营销平台 - 抽奖展示',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
